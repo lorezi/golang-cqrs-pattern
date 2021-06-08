910c1f72db98ba3d577042e8f9a725f267fe3a25
Lawrence Onaulogho <onaulogholawrence@gmail.com>
2021-06-08 11:08:09 +0100

    adds utility function declaration for dealing with the event

--- event/event.go
+++ event/event.go
@@ -8,3 +8,23 @@ type EventStore interface {
 	SubscribeMeowCreated() (<-chan MeowCreatedMessage, error)
 	OnMeowCreated(f func(MeowCreatedMessage)) error
 }
+
+var impl EventStore
+
+func SetEventStore(es EventStore) {
+	impl = es
+}
+
+func Close() {
+	impl.Close()
+}
+
+func PublishMeowCreated(meow schema.Meow) error {
+	return impl.PublishMeowCreated(meow)
+}
+func SubscribeMeowCreated() (<-chan MeowCreatedMessage, error) {
+	return impl.SubscribeMeowCreated()
+}
+func OnMeowCreated(f func(MeowCreatedMessage)) error {
+	return impl.OnMeowCreated(f)
+}
