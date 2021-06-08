555fcedea146ace24f7d24c16c2f783d7857806e
Lawrence Onaulogho <onaulogholawrence@gmail.com>
2021-06-08 11:03:47 +0100

    adds event messages

---
+++ event/event.go
@@ -0,0 +1,10 @@
+package event
+
+import "github.com/lorezi/golang-cqrs-pattern/schema"
+
+type EventStore interface {
+	Close()
+	PublishMeowCreated(meow schema.Meow) error
+	SubscribeMeowCreated() (<-chan MeowCreatedMessage, error)
+	OnMeowCreated(f func(MeowCreatedMessage)) error
+}
---
+++ event/messages.go
@@ -0,0 +1,17 @@
+package event
+
+import "time"
+
+type Message interface {
+	Key() string
+}
+
+type MeowCreatedMessage struct {
+	ID        string
+	Body      string
+	CreatedAt time.Time
+}
+
+func (m *MeowCreatedMessage) Key() string {
+	return "meow.created"
+}
