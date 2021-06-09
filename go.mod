d79b844cbe58f2d1e73ebb952f0a1e0fb82b67a3
Lawrence Onaulogho <onaulogholawrence@gmail.com>
2021-06-09 17:12:23 +0100

    adds elasticsearch interface implementation

--- go.mod
+++ go.mod
@@ -2,4 +2,9 @@ module github.com/lorezi/golang-cqrs-pattern
 
 go 1.15
 
-require github.com/nats-io/nats.go v1.11.0
+require (
+	github.com/mailru/easyjson v0.7.7 // indirect
+	github.com/nats-io/nats.go v1.11.0
+	github.com/olivere/elastic v6.2.35+incompatible
+	github.com/pkg/errors v0.9.1 // indirect
+)
---
+++ search/elastic.go
@@ -0,0 +1,57 @@
+// elastic search implementation
+package search
+
+import (
+	"context"
+	"encoding/json"
+	"log"
+
+	"github.com/lorezi/golang-cqrs-pattern/schema"
+	"github.com/olivere/elastic"
+)
+
+type ElasticRepository struct {
+	client *elastic.Client
+}
+
+func NewElastic(url string) (*ElasticRepository, error) {
+	client, err := elastic.NewClient(elastic.SetURL(url), elastic.SetSniff(false))
+
+	if err != nil {
+		return nil, err
+	}
+
+	return &ElasticRepository{client}, nil
+}
+
+func (r *ElasticRepository) Close() {
+
+}
+
+func (r *ElasticRepository) InsertMeow(ctx context.Context, meow schema.Meow) error {
+	_, err := r.client.Index().Index("meows").Type("meow").Id(meow.ID).BodyJson(meow).Refresh("wait_for").Do(ctx)
+
+	return err
+}
+
+func (r *ElasticRepository) SearchMeows(ctx context.Context, query string, skip uint64, take uint64) ([]schema.Meow, error) {
+	result, err := r.client.Search().Index("meows").Query(
+		elastic.NewMultiMatchQuery(query, "body").Fuzziness("3").PrefixLength(1).CutoffFrequency(0.0001),
+	).From(int(skip)).Size(int(take)).Do(ctx)
+
+	if err != nil {
+		return nil, err
+	}
+
+	meows := []schema.Meow{}
+
+	for _, hit := range result.Hits.Hits {
+		var meow schema.Meow
+		if err = json.Unmarshal(*hit.Source, &meow); err != nil {
+			log.Println(err)
+		}
+		meows = append(meows, meow)
+	}
+
+	return meows, nil
+}
